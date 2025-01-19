1e68009f4669f74cfc3d072e0cd278ddca5032a6
leuchtstoffratte <kitersquirel@web.de>
2025-01-19 23:41:32 +0100

    Restructured pizza-selection

--- src/main/java/com/rattencorp/pizzajsp/controller/PizzaSelectionController.java
+++ src/main/java/com/rattencorp/pizzajsp/controller/PizzaSelectionController.java
@@ -7,33 +7,64 @@ import com.rattencorp.pizzajsp.model.order.CurrentOrder;
 import com.rattencorp.pizzajsp.model.order.OrderedPizza;
 import jakarta.enterprise.context.SessionScoped;
 import jakarta.inject.Inject;
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.IOException;
 
 
-@WebServlet(urlPatterns = "/select-pizzas")
+@WebServlet(urlPatterns = "/select-pizzas-controller")
 @SessionScoped
 public class PizzaSelectionController extends HttpServlet {
 
-    private final PizzaMenu menu;
-
-    private final CurrentOrder currentOrder;
+    private static final Logger LOGGER = LogManager.getLogger(PizzaSelectionController.class);
 
     @Inject
-    public PizzaSelectionController(PizzaMenu menu, CurrentOrder currentOrder){
-        this.menu = menu;
-        this.currentOrder = currentOrder;
-    }
+    private PizzaMenu menu;
 
+    @Inject
+    private CurrentOrder currentOrder;
+
+
+
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response){
+
+
+
+        LOGGER.error("request: {}", request);
+        LOGGER.error("pizzas: {}", currentOrder.getPizzas());
+
+
+/*        RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/");
+        try {
+            dispatcher.forward(request, response);
+        } catch (ServletException | IOException e) {
+            throw new RuntimeException(e);
+        }
+*/    }
 
     @Override
     public void doPost(HttpServletRequest request, HttpServletResponse response){
 
 
 
+        LOGGER.error("request: {}", request);
+        LOGGER.error("pizzas: {}", currentOrder.getPizzas());
 
-    }
+
+  /*      RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/");
+        try {
+            dispatcher.forward(request, response);
+        } catch (ServletException | IOException e) {
+            throw new RuntimeException(e);
+        }
+  */  }
 
 }
--- src/main/webapp/PizzaSelection.jsp
+++ src/main/webapp/PizzaSelection.jsp
@@ -1,20 +1,19 @@
-<%@ page import="com.rattencorp.pizzajsp.model.menue.PizzaType" %>
 <%@ page import="com.rattencorp.pizzajsp.view.PizzaPresentation" %>
 <%@include file="common/PizzaHeader.jspf"%>
 
 <jsp:useBean id="pizzaMenu" class="com.rattencorp.pizzajsp.service.PizzaMenuService" scope="application"/>
 <jsp:useBean id="currentOrder" class="com.rattencorp.pizzajsp.model.order.CurrentOrder" scope="session"/>
-
 <div id="pizza-menu">
+    <h1>${pageContext.request.contextPath}</h1>
 
     <%
         for(PizzaPresentation pizza : pizzaMenu.supplyMenu()){
     %>
         <%@include file="pizza/PizzaDisplay.jspf" %>
     <% } %>
-<form action="com.rattencorp.pizzajsp.controller.PizzaSelectionController">
-    <input type="submit" value="Bestellung abschicken">
-</form>
+    <form action="${pageContext.request.contextPath}/accept-address" method="get">
+        <input type="submit" value="Bestellung abschicken">
+    </form>
 </div>
 
 <div id="current-order">
